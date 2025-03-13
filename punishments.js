8cb89e8ec6f9b5659b9a13898c129b993acc6b9c
huibertt <dennis87767@gmail.com>
2025-03-13 14:04:16 +0100

    Update punishments.js

--- punishments.js
+++ punishments.js
@@ -1,14 +1,14 @@
 
 export const punishmentCategories = [
-    "Geen strafcategorie (placeholder)",
-    "Categorie 1: Melding of waarschuwing",
-    "Categorie 2: Tijdelijke ban van 24 uur",
-    "Categorie 3: Tijdelijke ban van 3 dagen",
-    "Categorie 4: Tijdelijke ban van 7 dagen",
-    "Categorie 5: Tijdelijke ban van 14 dagen",
-    "Categorie 6: Tijdelijke ban van 30 dagen",
-    "Categorie 7: Permanente ban (eventueel te bespreken na 6 maanden)",
-    "Categorie 8: Permanente ban zonder mogelijkheid tot discussie",
-    "Categorie 9: Permanente ban bij extreme overtredingen",
-    "Categorie 10: Speciale categorie (zoals IRL trade of extreme toxiciteit)"
+    "Categorie 0: Geen straf. (placeholder)",
+    "Categorie 1: Warn / ban tot 8 uur / adminjail",
+    "Categorie 2: Ban van 24 uur",
+    "Categorie 3: Ban van 2 dagen",
+    "Categorie 4: Ban van 3 dagen",
+    "Categorie 5: Ban van 7 dagen",
+    "Categorie 6: Ban van 14 dagen",
+    "Categorie 7: Ban van 1 maand",
+    "Categorie 8: Permanente ban",
+    "Categorie 9: Account-wipe",
+    "Categorie 10: Geen unban mogelijk (GUM) + discord ban"
 ];
