1bc8c172f806aaf91fbe6e7b27c00622a40972e7
huibertt <dennis87767@gmail.com>
2025-03-13 09:47:19 +0100

    Add files via upload

---
+++ articles.js
@@ -0,0 +1,18 @@
+
+import { punishmentCategories } from './punishments.js';
+
+export const articles = [
+    {
+        label: "Artikel 1: Voorbeeld",
+        description: "Dit is een voorbeeldartikel.",
+        punishmentCategory: 1,
+        subarticles: [
+            {
+                label: "Artikel 1A: Subartikel voorbeeld",
+                description: "Subartikel beschrijving",
+                punishmentCategory: 2
+            }
+        ]
+    }
+    // Hier jouw volledige artikelen array plakken.
+];
