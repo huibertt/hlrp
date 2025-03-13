a0f5d4eca2eb505ee361edcf280493581a62b9e1
huibertt <dennis87767@gmail.com>
2025-03-13 16:16:38 +0100

    Update articles.js

--- articles.js
+++ articles.js
@@ -77,15 +77,17 @@ export const articles = [
         list: [
             "Het roleplay scenario vermijden tijdens ernstige verwondingen met andere burgers of overheidsdiensten.",
             "Het roleplay scenario vermijden tijdens ernstige aanrijdingen met andere burgers of overheidsdiensten test 1.",
-            { sublist: [
-                "nige bedoeling zelf te gaan spelen.",
-                "oel te hebben langer te gaan spelen."
+            { 
+                sublist: [
+                    "nige bedoeling zelf te gaan spelen.",
+                    "oel te hebben langer te gaan spelen."
                 ]
             },
             "Het roleplay scenario vermijden tijdens ernstige verwondingen met andere burgers of overheidsdiensten.",
             "Het roleplay scenario vermijden tijdens ernstige aanrijdingen met andere burgers of overheidsdiensten."
-        ],        
+        ]
     }
+
 
 
 
