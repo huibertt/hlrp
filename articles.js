0e766fbe4966485c516279969b1246e11738d7c6
huibertt <dennis87767@gmail.com>
2025-03-13 11:00:10 +0100

    Update articles.js

--- articles.js
+++ articles.js
@@ -12,6 +12,28 @@ export const articles = [
                 description: "Subartikel beschrijving",
                 punishmentCategory: 2
             }
+
+    {
+        label: "Artikel 1: Voorbeeld",
+        description: "Dit is een voorbeeldartikel.",
+        punishmentCategory: 1,
+        subarticles: [
+            {
+                label: "Artikel 1A: Subartikel voorbeeld",
+                description: "Subartikel beschrijving",
+                punishmentCategory: 2
+            }
+
+    {
+        label: "Artikel 1: Voorbeeld",
+        description: "Dit is een voorbeeldartikel.",
+        punishmentCategory: 1,
+        subarticles: [
+            {
+                label: "Artikel 1A: Subartikel voorbeeld",
+                description: "Subartikel beschrijving",
+                punishmentCategory: 2
+            }
         ]
     }
     // Hier jouw volledige artikelen array plakken.
